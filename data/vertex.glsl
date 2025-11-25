cb9c7e2d3e145746b931252045575cb7c291f13e
Jared Bruni <jaredbruni@gmail.com>
2025-11-25 06:36:34 -0800

    updated vertex shader

--- data/vertex.glsl
+++ data/vertex.glsl
@@ -3,6 +3,7 @@ layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 normal;
 layout (location = 2) in vec2 texCoord;
 
+out vec3 vNormal;
 out vec2 tc;
 
 uniform mat4 mv_matrix;
@@ -10,5 +11,6 @@ uniform mat4 proj_matrix;
 
 void main() {
     gl_Position = proj_matrix * mv_matrix * vec4(position, 1.0);
+    vNormal = normal;
     tc = texCoord;
 }
